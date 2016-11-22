153992e28910d10716ebf745d5277b677455cdc7
Roberto Flores <robertoflores@Robertos-MBP.sparks42.com>
2016-11-22 20:13:08 +0100

    Set up databases started working on ew templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
+# Import Category Model
+from rango.models import Category
 
 
 # Create your views here.
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context
-    # Note the key boldmessage is the same as {{ boldmessage }} in the index
-    # template
+    # Query the database for a list of ALL categories currently stored
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in a context dictionary.
+    # that will be passed to the template engine
 
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut to make our lives easier.
-    # Note that the first parameter is the template we wish to use
+    # In the '-likes' parameter, the '-' stands for descending
+    # [:5] splits the list and takes the elements from 0 to 4
 
+    # Return the reponse and send it back!
     return render(request, 'rango/index.html', context=context_dict)
