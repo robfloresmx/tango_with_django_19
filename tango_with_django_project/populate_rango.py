409b41e85f1f56a5a3afdbd6a9ef5f43f7df7a4f
Roberto Flores <robertoflores@Robertos-MBP.sparks42.com>
2016-11-24 16:40:31 +0100

    Added Top 5 Pages to home page.

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,4 +1,5 @@
 import os
+from random import randint
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
@@ -17,26 +18,34 @@ def populate():
 
     python_pages = [
         {"title": "Official Python Tutorial",
-         "url": "http://docs.python.org/2/tutorial/"},
+         "url": "http://docs.python.org/2/tutorial/",
+         "views": 10},
         {"title": "How to Think Like a Computer Scientist",
-         "url": "http://www.greenteapress.com/thinkpython/"},
+         "url": "http://www.greenteapress.com/thinkpython/",
+         "views": 12},
         {"title": "Learn Python in 10 Minutes",
-         "url": "http://www.korokithakis.net/tutorials/python/"}
+         "url": "http://www.korokithakis.net/tutorials/python/",
+         "views": 9}
     ]
 
     django_pages = [
         {"title": "Official Django Tutorial",
-         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 13},
         {"title": "Django Rocks",
-         "url": "http://www.djangorocks.com/"},
+         "url": "http://www.djangorocks.com/",
+         "views": 10},
         {"title": "How to Tango with Django",
-         "url": "http://www.tangowithdjango.com/"}]
+         "url": "http://www.tangowithdjango.com/",
+         "views": 8}]
 
     other_pages = [
         {"title": "Bottle",
-         "url": "http://bottlepy.org/docs/dev/"},
+         "url": "http://bottlepy.org/docs/dev/",
+         "views": 10},
         {"title": "Flask",
-         "url": "http://flask.pocoo.org"}]
+         "url": "http://flask.pocoo.org",
+         "views": 30}]
 
     cats = {"Python": {"pages": python_pages},
             "Django": {"pages": django_pages},
@@ -54,7 +63,7 @@ def populate():
         c = add_cat(cat)
         if __name__ == '__main__':
             for p in cat_data["pages"]:
-                add_page(c, p["title"], p["url"])
+                add_page(c, p["title"], p["url"], p["views"])
 
     # Print out the categories we have added
     for c in Category.objects.all():
@@ -62,7 +71,7 @@ def populate():
             print("- {0} - {1}".format(str(c), str(p)))
 
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
     p.views = views
@@ -71,7 +80,9 @@ def add_page(cat, title, url, views=0):
 
 
 def add_cat(name):
+    likes = randint(0, 50)
     c = Category.objects.get_or_create(name=name)[0]
+    setattr(c, 'likes', likes)
     c.save()
     return c
 
--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
 ]
